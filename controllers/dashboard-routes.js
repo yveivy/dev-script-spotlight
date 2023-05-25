4ed470c3c72b74fa4c7424b459a3b9bde43881b6
Valais Girl <Yevette@Hal-71.local>
2023-05-25 16:11:57 -0500

    dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', withAuth, (req, res) => {
+
+    Post.findAll({
+        where: {
+            user_id: req.sessions.user_id
+        }, 
+        attributes: [ 'id', 'title', 'body', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+
+            }
+        ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true}));
+        res.render('dashboard', { posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: ['id', 'title', 'body','created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(postData => {
+        if(postData) {
+            const post = postData.get({ plain: true});
+            
+            res.render('edit-post', {
+                post, loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
