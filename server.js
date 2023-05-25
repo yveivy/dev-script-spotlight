8aab5f2e98bd73118f54102c4b42b769abc9e04a
Valais Girl <Yevette@Hal-71.local>
2023-05-25 17:42:57 -0500

    debug connection

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const express = require('express-session');
-const session = require('express-handlebars');
-const routes = require('/controllers');
-const helpers = require('.utils/helpers');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -34,7 +35,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path.join(__diname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
