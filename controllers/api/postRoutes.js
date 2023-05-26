aea602ddb5374c3bf75c3e31dc5986055d6c9951
Valais Girl <Yevette@Hal-71.local>
2023-05-26 18:24:37 -0500

    fixed typos

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
@@ -62,62 +63,62 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// router.post('/', withAuth, async (req, res) => {
-//     try {
-//         const newPost = await Post.create({
-//             ...req.body,
-//             user_id: req.session.user_id,
-//         });
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
-//         res.status(200).json(newPost);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// router.put('/:id', withAuth, (req, res) => {
-//     Post.update(
-//         {
-//             title: req.body.title,
-//             body: req.body.body
-//         },
-//         {
-//             where: {
-//                 id: req.params.id
-//             }
-//         }
+router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            body: req.body.body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
 
-//     )
-//     .then(postData => {
-//         if (!postData) {
-//             res.status(404).json({ message: 'No post found with this id'});
-//             return;
-//         }
-//         res.json(postData);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+    )
+    .then(postData => {
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        res.json(postData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//     try {
-//         const postData = await belongsTo.destroy({
-//             where: {
-//                 id: req.params.id,
-//                 user_id: req. session.user_id,
-//             },
-//         });
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await belongsTo.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req. session.user_id,
+            },
+        });
 
-//         if(!postData) {
-//             res.status(404).json({ message: 'No post found with this id!'});
-//             return;
-//         }
-//         res.status(200).json(postData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        if(!postData) {
+            res.status(404).json({ message: 'No post found with this id!'});
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
