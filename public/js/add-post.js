5ce52bc2c1fd9efe2d2637422e5830d7feec27af
Valais Girl <Yevette@Hal-71.local>
2023-05-25 19:31:27 -0500

    js render elements on page

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,24 +1,24 @@
-async function newFromHandler(event) {
-    event.preventDefault();
+// async function newFromHandler(event) {
+//     event.preventDefault();
 
-    const title = document.querySelector('input [name="post-title"]').ariaValueMax;
-    const body = document.querySelector('textarea[name="post-body]').ariaValueMax;
+//     const title = document.querySelector('input [name="post-title"]').value;
+//     const body = document.querySelector('textarea[name="post-body]').value;
 
-    const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title,body
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+//     const response = await fetch('/api/posts', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title,body
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
-}
+//     if (response.ok) {
+//         document.location.replace('/dashboard');
+//     } else {
+//         alert(response.statusText);
+//     }
+// }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+// document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
