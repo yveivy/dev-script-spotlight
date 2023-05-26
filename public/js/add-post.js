1985f158fdbad6ac6eeeb0b735e8993fea36f479
Valais Girl <Yevette@Hal-71.local>
2023-05-25 19:07:38 -0500

    add-post render javascript

---
+++ public/js/add-post.js
@@ -0,0 +1,24 @@
+async function newFromHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input [name="post-title"]').ariaValueMax;
+    const body = document.querySelector('textarea[name="post-body]').ariaValueMax;
+
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
