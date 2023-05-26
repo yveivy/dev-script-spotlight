5176a7f9b3a39a51fef660297b1db3fb0322303b
Valais Girl <Yevette@Hal-71.local>
2023-05-25 19:05:54 -0500

    following bugs through the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('.postRoutes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-routers.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('.homeRoutes');
+const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/', homeRoutes);
